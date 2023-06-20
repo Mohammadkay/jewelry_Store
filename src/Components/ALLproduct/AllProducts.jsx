9423e9e77f57b25fa29986378e0eb1e6a068c5b5
[Mohammadkay] <[mohamd.al.alkayyali@gmail.com]>
2023-06-20 19:36:28 +0300

    Start with AllProduct + update on json server

---
+++ src/Components/ALLproduct/AllProducts.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+export default function AllProducts() {
+  useEffect(() => {
+    fetch("http://localhost:9000/jewelryArray")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+    return <div>
+      
+  </div>;
+}
