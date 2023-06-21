29f06f79c66c882b1428503eeb3bdcfaf9886082
[Mohammadkay] <[mohamd.al.alkayyali@gmail.com]>
2023-06-21 11:30:39 +0300

    fix json server and upload sample of work

--- src/Components/ALLproduct/AllProducts.jsx
+++ src/Components/ALLproduct/AllProducts.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import "./AllProducts.css";
 export default function AllProducts() {
+  const [products, setProduct] = useState([]);
   useEffect(() => {
     fetch("http://localhost:9000/jewelryArray")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      });
   }, []);
-    return <div>
-      
-  </div>;
+
+  return (
+    <section className="cards">
+      {products.map((ele) => {
+        return (
+          <article key={ele.id} >
+            <div>
+              <img src={ele.image} alt={ele.name} />
+            </div>
+            <div>
+              <h4>{ele.name}</h4>
+              <h4>{ele.price}</h4>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
