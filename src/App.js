e1cf760da4aaffb8d3c659bf4b360691e9800140
duhamjilani <duha.m.jilani@gmail.com>
2023-06-24 14:08:21 +0300

    after edits

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ import Nav from "./Components/NavBar/Nav";
 import OurTeamCountainer from "./Landing page/OurTeam/OurTeamCountainer";
 import IndividualIntervalsExample from "./Landing page/Sliderimg/Sliderimg";
 import TestimonialCountainer from "./Landing page/Testimonial/TestimonialCountainer"
->>>>>>> 2c75d5e69c1f816016bf7c1f3d176a24390858ea
+
 function App() {
   return (
     <>
-      <Login />
-      <Register />
+    
     </>
   );
 }
