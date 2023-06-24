b0995dd15c3486c6fd8bb001ee85f953bb0bb096
duhamjilani <duha.m.jilani@gmail.com>
2023-06-24 13:53:55 +0300

    second change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 
+import AboutUs from'./Components/AboutUs/AboutUs'
+import Categories from './Components/Category/Categories';
 
 
 
-import "./App.css";
-
-// import AllProducts from "./Components/ALLproduct/AllProducts";
-import AboutUs from "./Components/AboutUs/AboutUs";
-import Categories from "./Components/Category/Categories";
-import Nav from "./Components/NavBar/Nav";
-import OurTeamCountainer from "./Landing page/OurTeam/OurTeamCountainer";
-import IndividualIntervalsExample from "./Landing page/Sliderimg/Sliderimg";
-import TestimonialCountainer from "./Landing page/Testimonial/TestimonialCountainer"
 
 function App() {
   return (
     <>
-      <Login />
-      <Register />
-    </>
+    {/* <AboutUs/> */}
+   <Categories/>
+     </>
   );
 }
 export default App;
