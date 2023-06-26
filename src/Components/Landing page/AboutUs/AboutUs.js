6f0941c1cb512f06faa9baabbf38c46d4e80874d
[Mohammadkay] <[mohamd.al.alkayyali@gmail.com]>
2023-06-26 16:28:25 +0300

    fix about text size

--- src/Components/Landing page/AboutUs/AboutUs.js
+++ src/Components/Landing page/AboutUs/AboutUs.js
@@ -5,20 +5,19 @@ function AboutUs() {
   return (
     <div>
       <div className="Center" id="About">
-        <img src="./image/Pic.jpg" alt=""  />
+        <img src="./image/Pic.jpg" alt="" />
         <div className="Text">
-          <h1 className="JewelleryStore">Jewelers Store </h1>   
-            <p>
-              Jewelers Store is one of the chief precious stone adornments
-              producers. Serving more than 5000 clients crosswise over the USA,
-              UK, Latin America, Australia, and Canada.By owning all parts of
-              the store network, including precious stone sourcing, cutting,
-              combination, and gems fabricating.Jewellery Store is resolved to
-              give the most ideal incentive to Jewelers all through the world.
-              We will probably enable the autonomous gem dealer to remain in
-              front of their
-            </p>
-        
+          <h1 className="JewelleryStore">Jewelers Store </h1>
+          <p>
+            Jewelers Store is one of the chief precious stone adornments
+            producers. Serving more than 5000 clients crosswise over the USA,
+            UK, Latin America, Australia, and Canada.By owning all parts of the
+            store network, including precious stone sourcing, cutting,
+            combination, and gems fabricating.Jewellery Store is resolved to
+            give the most ideal incentive to Jewelers all through the world. We
+            will probably enable the autonomous gem dealer to remain in front of
+            their
+          </p>
         </div>
       </div>
     </div>
